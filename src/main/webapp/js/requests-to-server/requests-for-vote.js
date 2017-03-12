ed83a19a05c347b6438665c5595b636effa9dc69
Mello007 <sindikat008@gmail.com>
2017-03-12 10:13:40 +0300

    optimized js code

--- src/main/webapp/js/requests-to-server/requests-for-vote.js
+++ src/main/webapp/js/requests-to-server/requests-for-vote.js
@@ -1,9 +1,9 @@
 function getDataAboutVote() {
     var idStructure = window.location.href.split('?')[1];
-    var priceRequest = new XMLHttpRequest();
+    var requestToServer = new XMLHttpRequest();
     var status;
-    priceRequest.open("GET", "/voting/" + idStructure, true);   //Указываем адрес GET-запроса
-    priceRequest.onload = function (item){
+    requestToServer.open("GET", "/voting/" + idStructure, true);   //Указываем адрес GET-запроса
+    requestToServer.onload = function (item){
         var values = JSON.parse(this.responseText);
         status = values['status'];
         if (status) {
@@ -13,35 +13,35 @@ function getDataAboutVote() {
             document.getElementById('name').innerHTML = 'Голосование закрыто!!!';
         }
     };
-    priceRequest.send(null);
+    requestToServer.send(null);
 }
 
 function getDataFromServer(idStructure) {
-    var priceRequest = new XMLHttpRequest();
-    priceRequest.open("GET", "/voting/" + idStructure + "/votes", true);   //Указываем адрес GET-запроса
-    priceRequest.onload = function (){             //Функция которая отправляет запрос на сервер для получения всех студентов
+    var requestToServer = new XMLHttpRequest();
+    requestToServer.open("GET", "/voting/" + idStructure + "/votes", true);   //Указываем адрес GET-запроса
+    requestToServer.onload = function (){             //Функция которая отправляет запрос на сервер для получения всех студентов
         var parsedItem = JSON.parse(this.responseText);
         var itemsTable = document.getElementById('all-items-voting'); //получаем элемент по Id
         itemsTable.innerHTML = '';      //очищаем таблицу от устаревших данных
         parsedItem._embedded.vote.forEach(function(item)  {
             console.log(item);
-            var itemNameElement = document.createElement('td'); //создаем элемент ячейку с названием для таблицы
-            itemNameElement.innerHTML =  item['name'] ;     //внедряем название предмета, полученное с сервера
-            var itemPriceElement = document.createElement('td');
-            itemPriceElement.innerHTML = item['count'];
+            var name = document.createElement('td'); //создаем элемент ячейку с названием для таблицы
+            name.innerHTML =  item['name'] ;     //внедряем название предмета, полученное с сервера
+            var count = document.createElement('td');
+            count.innerHTML = item['count'];
             var operations = document.createElement('td');
             operations.innerHTML =
                 ' <button class="btn btn-primary btn-xs" onclick="addVoteItem(\'' + item['name'] + '\')">Голосовать </button> ';
             var elementRow = document.createElement('tr'); /// /создаем строку таблицы
-            elementRow.appendChild(itemNameElement);      //помещаем обе ячейки в строку
-            elementRow.appendChild(itemPriceElement);
+            elementRow.appendChild(name);      //помещаем обе ячейки в строку
+            elementRow.appendChild(count);
             elementRow.appendChild(operations);
             itemsTable.appendChild(elementRow);           //помещаем строку в таблицу
         });
         $('table').filterTable({minRows: 0});
         $("#all-items-table").tablesorter();
     };
-    priceRequest.send(null);
+    requestToServer.send(null);
 }
 
 function addVoteItem(name) {
--- src/main/webapp/js/requests-to-server/requests-for-voting.js
+++ src/main/webapp/js/requests-to-server/requests-for-voting.js
@@ -29,13 +29,13 @@ function viewStatistics(pathToServer) {
         itemsTable.innerHTML = '';      //очищаем таблицу от устаревших данных
         parsedItem._embedded.vote.forEach(function(item)  {
             console.log(item);
-            var itemNameElement = document.createElement('td'); //создаем элемент ячейку с названием для таблицы
-            itemNameElement.innerHTML =  item['name'] ;     //внедряем название предмета, полученное с сервера
-            var itemPriceElement = document.createElement('td');
-            itemPriceElement.innerHTML = item['count'];
+            var name = document.createElement('td'); //создаем элемент ячейку с названием для таблицы
+            name.innerHTML =  item['name'] ;     //внедряем название предмета, полученное с сервера
+            var count = document.createElement('td');
+            count.innerHTML = item['count'];
             var elementRow = document.createElement('tr'); /// /создаем строку таблицы
-            elementRow.appendChild(itemNameElement);      //помещаем обе ячейки в строку
-            elementRow.appendChild(itemPriceElement);
+            elementRow.appendChild(name);      //помещаем обе ячейки в строку
+            elementRow.appendChild(count);
             itemsTable.appendChild(elementRow);           //помещаем строку в таблицу
         });
         $('table').filterTable({minRows: 0});
@@ -98,17 +98,17 @@ $(function(){
 
 $(document).ready(function() {
     var requestToServer = new XMLHttpRequest();
-    requestToServer.open("GET", "/voting/", true);   //Указываем адрес GET-запроса
-    requestToServer.onload = function (){             //Функция которая отправляет запрос на сервер для получения всех студентов
+    requestToServer.open("GET", "/voting/", true);
+    requestToServer.onload = function (){
         var parsedItem = JSON.parse(this.responseText);
-        var itemsTable = document.getElementById('all-items'); //получаем элемент по Id
-        itemsTable.innerHTML = '';      //очищаем таблицу от устаревших данных
+        var itemsTable = document.getElementById('all-items');
+        itemsTable.innerHTML = '';
         parsedItem._embedded.voting.forEach(function(item)  {
             var status = item['status'];
-            var itemNameElement = document.createElement('td'); //создаем элемент ячейку с названием для таблицы
-            itemNameElement.innerHTML =  item['name'] ;     //внедряем название предмета, полученное с сервера
-            var itemPriceElement = document.createElement('td');
-            itemPriceElement.innerHTML = '<a href=' + createLink(item['_links']['self']['href']) + '\>Ссылка на голосование</a>';
+            var name = document.createElement('td');
+            name.innerHTML =  item['name'] ;
+            var linkForVote = document.createElement('td');
+            linkForVote.innerHTML = '<a href=' + createLink(item['_links']['self']['href']) + '\>Ссылка на голосование</a>';
             var statusHtml = document.createElement('td');
             var operations = document.createElement('td');
             if (status == false){
@@ -124,12 +124,12 @@ $(document).ready(function() {
                     '<button class="btn btn-primary btn-xs" onclick="deleteVoting(\'' + item['name'] + '\')">Удалить</button>'+
                     '<button class="btn btn-primary btn-xs" onclick="setStatusClose(\'' + item['name'] + '\')">Закрыть голосование</button> </div>';
             }
-            var elementRow = document.createElement('tr'); /// /создаем строку таблицы
-            elementRow.appendChild(itemNameElement);      //помещаем обе ячейки в строку
-            elementRow.appendChild(itemPriceElement);
+            var elementRow = document.createElement('tr');
+            elementRow.appendChild(name);
+            elementRow.appendChild(linkForVote);
             elementRow.appendChild(statusHtml);
             elementRow.appendChild(operations);
-            itemsTable.appendChild(elementRow);           //помещаем строку в таблицу
+            itemsTable.appendChild(elementRow);
         });
         $('table').filterTable({minRows: 0});
         $("#all-items-table").tablesorter();
